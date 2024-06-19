a55e9a8ffc29da1b02ef0a62d6df8a4ce68d88a2
BlackHat86 <suryansh.c05@gmail.com>
2024-06-19 17:01:28 +0530

    first commit

--- nav.js
+++ nav.js
@@ -1,12 +1,24 @@
 function loadNavbar() {
-    fetch('navbar.html')
+  fetch('navbar.html')
       .then(response => response.text())
       .then(data => {
-        document.getElementById('navbar').innerHTML = data;
+          document.getElementById('navbar').innerHTML = data;
+
+          // Retrieve the username from local storage
+          const username = localStorage.getItem('username');
+
+          // Check if the username is available
+          if (username) {
+              // Display the username
+              document.getElementById('username').textContent = `Hi, ${username}`;
+          } else {
+              // Optionally handle the case where the username is not found
+              // For example, redirect to login page
+              window.location.href = 'index.html'; // Adjust the path to your login page
+          }
       })
       .catch(error => console.error('Error loading navbar:', error));
-  }
+}
 
-  // Call the function to load the navbar
-  loadNavbar();
-  
+// Call the function to load the navbar
+loadNavbar();
