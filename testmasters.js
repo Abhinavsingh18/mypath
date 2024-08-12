29d5d5679068c72d489510e85c1e1acb3f96adf7
BlackHat86 <suryansh.c05@gmail.com>
2024-08-12 20:54:54 +0530

    commit

--- testmasters.js
+++ testmasters.js
@@ -524,21 +524,30 @@ function populateTable(tests) {
 }
 
 document.getElementById('priceForm').addEventListener('submit', function (event) {
-    event.preventDefault(); // Prevent the default form submission
+  event.preventDefault(); // Prevent the default form submission
 
-    const form = event.target;
-    const formData = new FormData(form);
+  const form = event.target;
+  const formData = new FormData(form);
 
-    fetch(form.action, {
-        method: form.method,
-        body: formData
-    })
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('responseMessage').innerHTML = data;
-    })
-    .catch(error => console.error('Error submitting form:', error));
+  // Log the FormData contents
+  formData.forEach((value, key) => {
+    console.log(`${key}: ${value}`);
+  });
+
+  fetch(form.action, {
+      method: form.method,
+      body: formData
+  })
+  .then(response => response.text())
+  .then(data => {
+      document.getElementById('responseMessage').innerHTML = data;
+  })
+  .catch(error => console.error('Error submitting form:', error));
 });
+
+
+
+
 });
 
 /////////
